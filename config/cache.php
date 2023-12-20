f0383f0dc5e85c3a90fe57b4e99a04ffbd485cf1
Pedro Pires <onelikeandidie@proton.me>
2023-12-20 11:21:49 +0000

    Added: Image caching header

--- config/cache.php
+++ config/cache.php
@@ -108,4 +108,8 @@ return [
 
     'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache_'),
 
+    'image' => [
+        // Default is 1 week
+        'expire' => env('IMAGE_CACHE_EXPIRE', 60 * 24 * 7),
+    ]
 ];
